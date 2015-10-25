f1dfd197c61eea81e2393751cc24686c3949a63d
Conor Branagan <conor.branagan@gmail.com>
2015-10-25 17:15:14 -0400

    refactor server to use classes

--- index.js
+++ index.js
@@ -1,74 +1,24 @@
-var server = require('http').createServer(),
-    url = require('url'),
-    WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({ server: server }),
-    express = require('express'),
-    app = express(),
-    port = 3000;
+"use strict"
 
-// Set up the templating.
+// Websocket Server
+var server = require('http').createServer();
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ server: server });
+
+// Chat Server
+var ChatServer = require('./chat-server').ChatServer;
+var cs = new ChatServer(wss).listen();
+
+// App initialization
+var express = require('express');
+var app = express();
+var port = 3000;
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.render('index');
-});
-
-var connSequence = 0,
-    activeConnections = {},
-    users = {};
-
-var MESSAGE_TYPES = {
-  INIT: 'init',
-  SET_USER: 'set_user',
-  MESSAGE: 'message',
-  USER_JOIN: 'user_join',
-  USER_LEAVE: 'user_leave'
-}
-
-var postMessage = function(message) {
-  for (k in activeConnections) {
-    activeConnections[k].send(JSON.stringify(message));
-  }
-}
-
-wss.on('connection', function connection(ws) {
-  var id = connSequence++;
-  activeConnections[id] = ws;
-  console.log('[id=%s] Opening connection', id);
-  ws.send(JSON.stringify({
-    type: MESSAGE_TYPES.INIT,
-    users: Object.keys(users).map(key => users[key])
-  }));
-
-  ws.on('message', function incoming(message) {
-    var msg = JSON.parse(message);
-    switch(msg.type) {
-      case MESSAGE_TYPES.SET_USER:
-        console.log('[id=%s] Setting username to %s', id, msg.username);
-        users[id] = {name: msg.username};
-        postMessage({
-          type: MESSAGE_TYPES.USER_JOIN,
-          user: users[id]
-        });
-        break;
-      case MESSAGE_TYPES.MESSAGE:
-        // TODO: Validate username.
-        postMessage(msg);
-        break;
-    }
-  });
-
-  ws.on('close', function() {
-    console.log('Closing connection id=%s', id);
-    delete activeConnections[id];
-    postMessage({
-      type: MESSAGE_TYPES.USER_LEAVE,
-      user: users[id]
-    });
-    delete users[id];
-  });
-
 });
 
 server.on('request', app);
